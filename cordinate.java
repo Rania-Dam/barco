a03112a1fbf4273fd870fb827693204d7425ae08
cronosgame <161036257+cronosgame@users.noreply.github.com>
2024-05-22 11:46:00 +0200

    comentarios

--- cordinate.java
+++ cordinate.java
@@ -1,17 +1,20 @@
 public class Coordinate {
-    private int row;
-    private int col;
+    private int row; // Almacena la fila de la coordenada.
+    private int col; // Almacena la columna de la coordenada.
 
+    // Constructor de la clase Coordinate que inicializa las variables de instancia.
     public Coordinate(int row, int col) {
-        this.row = row;
-        this.col = col;
+        this.row = row; // Inicializa la fila con el valor proporcionado.
+        this.col = col; // Inicializa la columna con el valor proporcionado.
     }
 
+    // Método para obtener el valor de la fila.
     public int getRow() {
-        return row;
+        return row; // Devuelve el valor de la fila.
     }
 
+    // Método para obtener el valor de la columna.
     public int getCol() {
-        return col;
+        return col; // Devuelve el valor de la columna.
     }
 }
